84397988c2872fcfd005dde054d5086dfa8074a6
thuya <thuyhatran@icloud.com>
2016-09-20 14:19:38 -0400

    Add buttons to move to 2 specific places

--- app/src/main/java/example/thuya/com/googlemaptestmontrealtosydney/MapsActivity.java
+++ app/src/main/java/example/thuya/com/googlemaptestmontrealtosydney/MapsActivity.java
@@ -2,7 +2,10 @@ package example.thuya.com.googlemaptestmontrealtosydney;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,6 +17,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    private Button b_toMontreal, b_toSydney;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +27,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        b_toMontreal = (Button) findViewById(R.id.toMontreal);
+
+        b_toSydney = (Button) findViewById(R.id.toSydney);
+
+        b_toMontreal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                LatLng montreal = new LatLng(45.52, -73.56);
+                mMap.addMarker(new MarkerOptions().position(montreal).title("Welcome to Montreal"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(montreal));
+
+                CameraUpdate zoom=CameraUpdateFactory.zoomTo(6);
+                mMap.animateCamera(zoom);
+
+            }
+        });
+
+
+        b_toSydney.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Add a marker in Sydney and move the camera
+
+                LatLng sydney = new LatLng(-34, 151);
+                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+
+                CameraUpdate sydneyLocation = CameraUpdateFactory.newLatLngZoom(sydney, 5);
+                mMap.animateCamera(sydneyLocation);
+                //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+            }
+        });
+
     }
 
 
@@ -38,9 +79,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in my home town and move the camera
+        LatLng hometown = new LatLng(16.34, 107.5);
+        mMap.addMarker(new MarkerOptions().position(hometown).title("Missing you, Hue City"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(hometown));
+
     }
 }
